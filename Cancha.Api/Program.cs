a31357eb719c53c1ee79d017c37aa94de6e7efd8
Mai200611 <penagarciam061@gmail.com>
2026-04-16 07:11:40 -0500

    Primer commit con todas las configuraciones hasta el migration

--- Cancha.Api/Program.cs
+++ Cancha.Api/Program.cs
@@ -1,6 +1,21 @@
+using System;
+using Cancha.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
+
+var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+if (builder.Environment.IsDevelopment())
+{
+    // fallback to LocalDB for local development (avoid exposing remote host-name issues)
+    conn = builder.Configuration.GetConnectionString("LocalDb") ?? "Server=(localdb)\\MSSQLLocalDB;Database=Cancha;Trusted_Connection=True;";
+}
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(conn, sqlOptions => 
+        sqlOptions.EnableRetryOnFailure(maxRetryCount:5, maxRetryDelay:TimeSpan.FromSeconds(30), errorNumbersToAdd:null)));
 
 var app = builder.Build();
 
