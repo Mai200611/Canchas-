d89507a43d3e58304eab00d9fe97e1954a51950a
CursedDaniel <cetroloco.daniel@gmail.com>
2026-05-02 00:11:44 -0500

    Se Crea El Controlador para Canchas, se  intenta resolver conflicto con los nombres y la palabra 'cancha' en la entidad

--- Cancha.Api/Data/DataContext.cs
+++ Cancha.Api/Data/DataContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Cancha.Shared.Entities;
-using CanchaEntidad = Cancha.Shared.Entities.Cancha; //Por conflicto con nombramiento
 
 namespace Cancha.Api.Data
 {
