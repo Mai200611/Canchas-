db78f24d2c22c2ca7627c3e40069677c483380b8
DylanITM <dylanarias1129116@correo.itm.edu.co>
2026-05-05 15:20:10 -0500

    Arregla puerto de salida y URI en proyecto WEB

--- Cancha.Web/Program.cs
+++ Cancha.Web/Program.cs
@@ -8,7 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000/")});
-builder.Services.AddScoped<IRepository, Repository>();
 await builder.Build().RunAsync();
 
 
