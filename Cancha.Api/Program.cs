d5c82f926c827df275ad4cbfdb5a368bcaf3fb03
YeferxonMarquez <yeferxonalejandromarquezaraque@gmail.com>
2026-05-04 21:10:24 -0500

    SeedDb para el proyecto Canchas

--- Cancha.Api/Program.cs
+++ Cancha.Api/Program.cs
@@ -4,23 +4,19 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
-
+builder.Services.AddRazorPages();
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+
+// Este es el registro del SeedDb
+builder.Services.AddTransient<SeedDb>();
+
 
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=DefaultConnection"));
 
 var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+
 if (builder.Environment.IsDevelopment())
 {
     // fallback to LocalDB for local development (avoid exposing remote host-name issues)
@@ -28,12 +24,20 @@ if (builder.Environment.IsDevelopment())
 }
 
 builder.Services.AddDbContext<DataContext>(options =>
-    options.UseSqlServer(conn, sqlOptions => 
-        sqlOptions.EnableRetryOnFailure(maxRetryCount:5, maxRetryDelay:TimeSpan.FromSeconds(30), errorNumbersToAdd:null)));
+    options.UseSqlServer(conn, sqlOptions =>
+        sqlOptions.EnableRetryOnFailure(maxRetryCount: 5, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null)));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Ejecucion del SeedDb
+var scopeFactory = app.Services.GetService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetService<SeedDb>();
+    await seeder.SeedAsync();
+}
+
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -45,8 +49,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
 
 app.MapControllers();
+app.UseRouting();
+app.UseAuthorization();
+app.MapStaticAssets();
+app.MapRazorPages()
+   .WithStaticAssets();
 
 app.Run();
